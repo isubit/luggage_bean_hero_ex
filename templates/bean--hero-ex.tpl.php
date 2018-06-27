ec14d8cbb06929c20bd52d7af4eaeddedc6eff8b
Carney, Sarah A <sacarney@iastate.edu>
2018-06-27 10:10:39 -0500

    LUGG-1107 Allow title and caption to be optional

--- templates/bean--hero-ex.tpl.php
+++ templates/bean--hero-ex.tpl.php
@@ -1,11 +1,16 @@
 <div class="bean-hero-ex <?php print $bean->field_hero_ex_position['und'][0]['value'] ?> <?php print $bean->field_hero_ex_color['und'][0]['value'] ?>" style="background-image: url(<?php print file_create_url($bean->field_hero_ex_image['und'][0]['uri']); ?>) ;" <?php print $attributes; ?>>
 
   <div class="bean-hero-ex_caption">
-    <div class="bean-hero-ex_content">
-      <?php if ($bean->title !== ''): ?>
-        <h2 class="bean-hero-ex_title"><?php print $title; ?></h2>
-      <?php endif; ?>
-      <?php print $bean->field_hero_ex_content['und'][0]['value']; ?>
-    </div>
+    <?php if (isset($bean->field_hero_ex_content['und'][0]['value']) or ($bean->title !== '')) : ?>
+      <div class="bean-hero-ex_content">
+        <?php if ($bean->title !== ''): ?>
+          <h2 class="bean-hero-ex_title"><?php print $title; ?></h2>
+        <?php endif; ?>
+
+        <?php if (isset($bean->field_hero_ex_content['und'][0]['value'])): ?>
+          <?php print $bean->field_hero_ex_content['und'][0]['value']; ?>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
